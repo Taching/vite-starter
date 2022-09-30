60c8ade53f3c25b9067f8a15d841620b15f28d51
stephen <stephn2027@gmail.com>
2022-09-30 15:18:20 +0900

    configured what-we-do to display content

---
+++ src/components/Slides/WhatWeDo/Steps/index.tsx
@@ -0,0 +1,13 @@
+export default function Steps() {
+  return (
+    <div className="outer">
+      <div className="progress">
+        <div className="right current">
+          <div>Intro</div>
+          <div>Ipsum dolor</div>
+          <div>Ament</div>
+        </div>
+      </div>
+    </div>
+  );
+}
